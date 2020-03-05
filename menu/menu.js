78995f310405de1df0dea54b2c09602ef5abc482
yonizaf <yonizaf@gmail.com>
2020-03-05 14:37:46 +0200

    replace icons with css font replace sync,checkmark emoji and external-link svg image with css font from FontAwesome

--- menu/menu.js
+++ menu/menu.js
@@ -2,9 +2,9 @@ const msSecond = 1000, msMinute = msSecond*60, msHour=msMinute*60,msDay=msHour*2
 function createRow(listItem){
 	let row = document.createElement("div");
 	let book = document.createElement("div");
-	let bookLink = document.createElement("img");
+	let bookLink = document.createElement("span");
 	let chapter = document.createElement("div");
-	let chapterLink = document.createElement("img");
+	let chapterLink = document.createElement("span");
 	let time = document.createElement("div");
 	row.className="row";
 	book.textContent=listItem.bookTitle;
@@ -12,8 +12,8 @@ function createRow(listItem){
 	chapter.title="Last Read: " + listItem.lastReadTitle;
 	bookLink.title=listItem.bookUrl;
 	chapterLink.title=listItem.chapUrl;
-	bookLink.src = chapterLink.src = "../icons/external.svg";
-	bookLink.className = chapterLink.className = "link";
+	bookLink.textContent = chapterLink.textContent = "\uF35D"; // alternative: \uF360
+	bookLink.className = chapterLink.className = "link fas";
 	time.textContent=timeAgo(listItem.timestamp,true);
 	time.title=listItem.timeText;
 	time.className="time";
